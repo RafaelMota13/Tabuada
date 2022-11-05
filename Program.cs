3f2eb106b8e8214021df6c151537f8e9caeb00f6
Rafael <faelmota7@gmail.com>
2022-11-04 22:34:38 -0300

    Resoluçao

--- Program.cs
+++ Program.cs
@@ -3,12 +3,12 @@ int  n = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine();
 int m = 0;
-int resul;
+
 
 while (m<=10)
 {
-    resul = n*m;
+   int resul = n * m;
     Console.WriteLine($"{n} x {m} = {resul}");
-     m = m+1;
+     m = m + 1;
 }
 Console.WriteLine();
