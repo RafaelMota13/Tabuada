b4da513ff408ab304fcbc8c078e15bed5258215b
Rafael <faelmota7@gmail.com>
2022-11-04 22:33:23 -0300

    Resoluçao

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("\nTábuada do número: ");
+int  n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+int m = 0;
+int resul;
+
+while (m<=10)
+{
+    resul = n*m;
+    Console.WriteLine($"{n} x {m} = {resul}");
+     m = m+1;
+}
+Console.WriteLine();
